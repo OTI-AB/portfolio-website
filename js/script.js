1b6808e93c01f66b2987bde0ab9f08a5de165a3e
OTI-AB <phillip.pearson@onsitetechnologies.caB>
2024-03-07 08:04:07 -0700

    Completed typing effect JS

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
 // Add custom JavaScript here
+function runTypingEffect() {
+	const text = 'My name is Phillip.';
+	const typingEl = document.getElementById('typing-text');
+	const typingDelay = 100;
+
+	typeText(text, typingEl, typingDelay);
+}
+
+function typeText(text, typingEl, typingDelay) {
+	for (let i = 0; i < text.length; i++) {
+		setTimeout(() => {
+			typingEl.textContent += text.charAt(i);
+		}, typingDelay * i);
+	}
+}
+
+document.addEventListener('DOMContentLoaded', runTypingEffect);
